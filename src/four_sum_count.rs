a42112a2eff8b642d54bf8e741b62d5cb45cacba
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-07-23 01:46:22 +0800

    update: optimize for data perf

--- src/four_sum_count.rs
+++ src/four_sum_count.rs
@@ -3,6 +3,7 @@ struct Solution;
 impl Solution {
     pub fn four_sum_count(a: Vec<i32>, b: Vec<i32>, c: Vec<i32>, d: Vec<i32>) -> i32 {
         let mut h = std::collections::HashMap::new();
+        h.reserve(2500);
         let mut ret = 0;
         for i in 0..a.len() {
             let ai = a[i];
@@ -15,9 +16,7 @@ impl Solution {
             let ci = c[i];
             for j in 0..d.len() {
                 let dj = d[j];
-                if let Some(c1) = h.get(&-(ci+dj)) {
-                    ret += c1;
-                }
+                ret += *h.get(&-(ci+dj)).unwrap_or(&0);
             }
         }
         ret
