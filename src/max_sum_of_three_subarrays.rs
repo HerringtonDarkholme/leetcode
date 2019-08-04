5c72aa4c6a816fcaca0fc53a9ae11a31551b0374
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-08-05 01:00:40 +0800

    update

--- src/max_sum_of_three_subarrays.rs
+++ src/max_sum_of_three_subarrays.rs
@@ -2,6 +2,7 @@ pub struct Solution;
 
 use std::collections::HashMap;
 
+// actually I can use mono stack for even faster!
 impl Solution {
     pub fn max_sum_of_three_subarrays(nums: Vec<i32>, k: i32) -> Vec<i32> {
         let k = k as usize;
@@ -24,24 +25,23 @@ impl Solution {
         let limit = sums.len() - k * (remaining - 1);
         while i <  limit {
             let mut ret = Solution::find_recur(i + k, k, remaining - 1, sums, hashes);
-            let current = sums[i] + ret.iter().map(|&i| sums[i]).sum::<i32>();
+            let tail_sum = ret.iter().map(|&i| sums[i]).sum::<i32>();
+            let mut current = sums[i];
             let next_i = if !ret.is_empty() {
-                let mut j = i + 1;
-                let mut mj = j;
-                let mut ms = sums[i];
+                let mut j = i;
                 while j < limit && j < ret[0] - k {
-                    if sums[j] > ms {
-                        ms = sums[j];
-                        mj = j;
+                    if sums[j] > current {
+                        current = sums[j];
+                        i = j;
                     }
                     j += 1;
                 }
-                mj
+                std::cmp::max(ret[0] - k, i + 1)
             } else {
                 i + 1
             };
-            if  current > max {
-                max = current;
+            if  current + tail_sum > max {
+                max = current + tail_sum;
                 ret.insert(0, i);
                 max_vec = ret;
             }
@@ -59,6 +59,7 @@ fn test() {
         (vec![3,43,4,2,2,6,7,3,23,3,5,6,8,9,34,23,4,4,6,8,4,233,54,5,34,34,4,56,45654], 3, vec![20, 23, 26]),
         (vec![3,3,4,6,2,3,5,8,2,5,3,6,8,3,7,8,9,9,3,2,1,5,5,6,8,2,1,2,3,5], 5, vec![9, 14, 21]),
         (vec![1,2,3], 1, vec![0, 1, 2]),
+        (vec![18,11,14,7,16,4,18,11,4,8], 2, vec![0, 3, 6]),
     ] {
         assert_eq!(Solution::max_sum_of_three_subarrays(i.0, i.1), i.2);
     }
