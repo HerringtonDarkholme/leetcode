9967f3ce310e37af6f39051161d5a0255b6bea23
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-08-10 21:18:18 +0800

    update

---
+++ src/n_repeated_element_times.rs
@@ -0,0 +1,24 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn repeated_n_times(a: Vec<i32>) -> i32 {
+        if a[0] == a[1] {
+            return a[0]
+        }
+        for i in 1..(a.len() / 2) {
+            let n2 = a[2*i];
+            let n3 = a[2*i + 1];
+            if n2 == n3 {
+                return n2
+            }
+            if n2 == a[2*i-1] || n2 == a[2*i-2] {
+                return n2
+            }
+            if n3 == a[2*i-1] || n3 == a[2*i-2] {
+                return n3
+            }
+        }
+        0
+        // a.windows(2).filter(|v| v[0] == v[1]).next().unwrap()[0]
+    }
+}
