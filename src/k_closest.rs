9cbc3095970e5bbb0716b0bc2c17f9bff8f79231
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-08-18 14:43:43 +0800

    update

--- src/k_closest.rs
+++ src/k_closest.rs
@@ -1,9 +1,15 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn k_closest(mut points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
+    pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
         let dist = |p: &Vec<i32>| {p[0] * p[0] + p[1] * p[1]};
-        points.sort_by_key(dist);
-        points.into_iter().take(k as usize).collect()
+        let mut p = points.iter()
+            .enumerate()
+            .map(|(k, p)| (k, dist(p)))
+            .collect::<Vec<_>>();
+        p.sort_by_key(|i| i.1);
+        p.into_iter().take(k as usize).map(|t| points[t.0].clone()).collect()
+
+        // points.into_iter().take(k as usize).collect()
     }
 }
