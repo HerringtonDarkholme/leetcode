48f117838e4921518736032fa0682d9c037e13b3
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-08-19 00:27:26 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 #[macro_use]
 mod util;
-pub mod orderly_queue;
+pub mod max_consecutive_ones_iii;
 
 fn main() {
 }
---
+++ src/max_consecutive_ones_iii.rs
@@ -0,0 +1,35 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn longest_ones(a: Vec<i32>, k: i32) -> i32 {
+        let k = k as usize;
+        let mut zeros: Vec<_> = a.iter().enumerate()
+            .filter_map(|(p, &i)| if i == 0 { Some(p) } else { None })
+            .collect();
+        if zeros.len() <= k {
+            return a.len() as i32
+        }
+        zeros.push(a.len());
+        let end = zeros.len() - k;
+        let mut max = 0;
+        let mut last_pos = 0;
+        for i in 0..end {
+            max = max.max(zeros[i + k] - last_pos);
+            last_pos = zeros[i] + 1;
+        }
+        max as i32
+    }
+}
+
+#[test]
+fn test() {
+    for t in vec![
+        (vec![1,1,1,0,0,0,1,1,1,1,0], 2, 6),
+        (vec![0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3, 10),
+        (vec![1,0,1,0,1,0], 3, 6),
+        (vec![1,1,1,0,0,0,1,1,1,1,0,0,0,1,1,0,1,0,1,0,1,0,1,0], 5, 13),
+        (vec![0,0,0,0,1,1,1,1,1,1,1,1,1,0,0], 3, 12),
+    ] {
+        assert_eq!(Solution::longest_ones(t.0, t.1), t.2);
+    }
+}
