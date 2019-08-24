cf081dcc47270e3d80fd8c3988194a058720a7ec
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-08-24 16:20:58 +0800

    update

--- src/backspace_compare.rs
+++ src/backspace_compare.rs
@@ -19,3 +19,27 @@ impl Solution {
         s1
     }
 }
+/*
+impl Solution {
+    pub fn backspace_compare(s: String, t: String) -> bool {
+        let mut s1 = Solution::resolve(s);
+        let mut t1 = Solution::resolve(t);
+        s1 == t1
+
+    }
+    fn resolve(s: String) -> Vec<char> {
+        let mut chars: Vec<_> = s.chars().collect();
+        let mut i = 0;
+        for j in 0..chars.len() {
+            if chars[j] == '#' {
+                i = if i > 0 { i - 1} else { 0 };
+            } else {
+                chars[i] = chars[j];
+                i += 1;
+            }
+        }
+        chars.resize(i, ' ');
+        chars
+    }
+}
+ **/
