12e7c07c56d09b058367e6ef841b92a51e4efb9f
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-09-02 00:20:07 +0800

    update

--- src/friend_circle.rs
+++ src/friend_circle.rs
@@ -1,27 +1,27 @@
 pub struct Solution;
 
+fn dfs(graph: &Vec<Vec<i32>>, visited: &mut Vec<bool>, start: usize) {
+    if visited[start] {
+        return;
+    }
+    visited[start] = true;
+    for (j, &friend) in graph[start].iter().enumerate() {
+        if friend == 1 {
+            dfs(graph, visited, j);
+        }
+    }
+}
+
 impl Solution {
     pub fn find_circle_num(m: Vec<Vec<i32>>) -> i32 {
-        let mut seen = std::collections::HashSet::new();
+        let mut visited = vec![false; m.len()];
         let mut ret = 0;
         for i in 0..m.len() {
-            if seen.contains(&i) {
+            if visited[i] {
                 continue;
             }
-            ret +=1;
-            seen.insert(i);
-            let mut stack = vec![i];
-            while !stack.is_empty() {
-                let j = stack.pop().unwrap();
-                for (k, &f) in m[j].iter().enumerate() {
-                    if seen.contains(&k) || f == 0 {
-                        continue;
-                    }
-                    seen.insert(k);
-                    stack.push(k);
-                }
-
-            }
+            dfs(&m, &mut visited, i);
+            ret += 1;
         }
         ret
     }
