ecb4763449e9bf02fb4b509f0a359742deff2ab5
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-09-14 12:09:28 +0800

    update

--- src/x-of-kind-in-a-deck-of-cards.rs
+++ src/x-of-kind-in-a-deck-of-cards.rs
@@ -22,3 +22,24 @@ impl Solution {
         false
     }
 }
+/*
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn has_groups_size_x(deck: Vec<i32>) -> bool {
+        let mut counter = HashMap::new();
+        let mut all = 0;
+        for i in deck {
+            all += 1;
+            *counter.entry(i).or_insert(0) += 1;
+        }
+        let mut occs = counter.values();
+        let first = *occs.next().unwrap();
+        occs.fold(first, |acc, &i| gcd(acc, i)) >= 2
+    }
+}
+
+fn gcd(x: i32, y: i32) -> i32 {
+    if x == 0 { y } else { gcd(y%x, x) }
+}
+*/
