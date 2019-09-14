cca47f5e2b9aac4867ea1e4e456b38003abb6c9d
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-09-14 11:54:29 +0800

    update

---
+++ src/x-of-kind-in-a-deck-of-cards.rs
@@ -0,0 +1,24 @@
+pub struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn has_groups_size_x(deck: Vec<i32>) -> bool {
+        let mut counter = HashMap::new();
+        let mut all = 0;
+        for i in deck {
+            all += 1;
+            *counter.entry(i).or_insert(0) += 1;
+        }
+        let occs: Vec<_> = counter.into_iter().map(|(k, v)| v).collect();
+        for x in 2..=all {
+            if all % x != 0 {
+                continue;
+            }
+            if occs.iter().all(|&o| o % x == 0) {
+                return true
+            }
+        }
+        false
+    }
+}
