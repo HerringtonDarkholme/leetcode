f19b4ff2375911d10a4f3f16df5c00fb4d9da7bf
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-10-23 00:21:02 +0800

    update

--- src/search_rotated.rs
+++ src/search_rotated.rs
@@ -5,6 +5,40 @@ impl Solution {
         if nums.is_empty() {
             return -1
         }
+        let mut low = 0;
+        let mut high = nums.len() - 1;
+        while low < high {
+            let mid = low + (high - low) / 2;
+            let e = nums[mid];
+            if e == target {
+                return mid as i32
+            }
+            let search_second = if e > nums[high] {
+                e <= target || target <= nums[high]
+            } else {
+                e <= target && target <= nums[high]
+            };
+            if search_second {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        if nums[low] == target {
+            low as i32
+        } else {
+            -1
+        }
+
+    }
+}
+
+/*
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        if nums.is_empty() {
+            return -1
+        }
         search_aux(&nums, target, 0, nums.len() - 1)
     }
 }
@@ -33,3 +67,4 @@ fn search_aux(nums: &Vec<i32>, target: i32, mut low: usize, mut high: usize) -> 
         search_aux(nums, target, mid + 1, high)
     }
 }
+*/
