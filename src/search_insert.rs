06ee98a0cb28782da6ffe159fafd056938dd8b47
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-10-25 00:29:36 +0800

    update

---
+++ src/search_insert.rs
@@ -0,0 +1,10 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        match nums.binary_search(&target) {
+            Ok(i) => i as i32,
+            Err(i) => i as i32,
+        }
+    }
+}
