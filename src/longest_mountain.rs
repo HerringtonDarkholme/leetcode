e8038de79a0ee7369d23191d185d0f649e133af2
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-10-27 17:13:17 +0800

    update

---
+++ src/longest_mountain.rs
@@ -0,0 +1,49 @@
+enum Mountain {
+    Initial,
+    Ascend,
+    Descend,
+}
+
+use self::Mountain::{Initial, Ascend, Descend};
+
+impl Solution {
+    pub fn longest_mountain(a: Vec<i32>) -> i32 {
+        let mut state = Initial;
+        let mut max = 0;
+        let mut left = 0;
+        for i in 1..a.len() {
+            let last = a[i - 1];
+            let curr = a[i];
+            if last > curr {
+                match state {
+                    Ascend => {
+                        state = Descend;
+                        max = max.max(i + 1 - left);
+                    },
+                    Descend => {
+                        max = max.max(i + 1 - left);
+                    },
+                    Initial => {
+                        left = i;
+                    },
+                }
+            } else if last < curr {
+                match state {
+                    Ascend => {},
+                    Descend => {
+                        state = Ascend;
+                        left = i - 1;
+                    },
+                    Initial => {
+                        state = Ascend;
+                        left = i - 1;
+                    },
+                }
+            } else {
+                left = i;
+                state = Initial;
+            }
+        }
+        max as i32
+    }
+}
