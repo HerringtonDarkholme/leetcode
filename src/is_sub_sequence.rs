b0d4bfe3aa24a0501a2162411e308a9922b38ec0
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-10-27 13:43:06 +0800

    update

--- src/is_sub_sequence.rs
+++ src/is_sub_sequence.rs
@@ -1,9 +1,9 @@
 pub struct Solution;
 impl Solution {
     pub fn is_subsequence(s: String, t: String) -> bool {
-        let t: Vec<_> = t.chars().collect();
+        let t = t.as_bytes();
         let mut i = 0;
-        for c in s.chars() {
+        for &c in s.as_bytes() {
             while i < t.len() && c != t[i] {
                 i += 1;
             }
