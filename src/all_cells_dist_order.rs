0c064541a546b2adfcb584df9dc4240f987716a2
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-11-06 00:46:56 +0800

    update

--- src/all_cells_dist_order.rs
+++ src/all_cells_dist_order.rs
@@ -2,13 +2,27 @@ pub struct Solution;
 
 impl Solution {
     pub fn all_cells_dist_order(r: i32, c: i32, r0: i32, c0: i32) -> Vec<Vec<i32>> {
-        let mut ret = vec![];
+        let mut ret = vec![vec![]; r*c];
+        // because we know max distance, we can use
+        // counting sort, count how many elemnts in one bucket
+        let mut counter = vec![0; r + c + 1];
         for x in 0..r {
             for y in 0..c {
-                ret.push(vec![x, y]);
+                let dist = (x - r0).abs() + (y - c0).abs();
+                counter[dist + 1] += 1;
             }
         }
-        ret.sort_by_key(|v| (v[0] - r0).abs() + (v[1] -c0).abs());
+        for i in 1..(r+c+1) {
+            // count offset
+            counter[i] += counter[i - 1];
+        }
+        for x in 0..r {
+            for y in 0..c {
+                let dist = (x - r0).abs() + (y - c0).abs();
+                ret[counter[dist]] = vec![x, y];
+                counter[dist] += 1;
+            }
+        }
         ret
     }
 }
