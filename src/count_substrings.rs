14ba2f8dee2bec5f5926c02f04f74ef486a22f3d
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-11-12 15:59:49 +0800

    remove vec usage

--- src/count_substrings.rs
+++ src/count_substrings.rs
@@ -5,21 +5,26 @@ impl Solution {
     pub fn count_substrings(s: String) -> i32 {
         let bytes = s.as_bytes();
         let len = bytes.len();
-        let mut candidates = vec![];
-        for i in 1..len {
-            candidates.push((i, 0));
-            candidates.push((i, 1));
-        }
-        candidates.pop();
         let mut ret = len as i32;
-        while !candidates.is_empty() {
-            let (i, l) = candidates.pop().unwrap();
-            if bytes[i - 1] != bytes[i + l] {
-                continue;
-            }
+        if len >= 2 && bytes[len - 1] == bytes[len - 2] {
             ret += 1;
-            if i - 1 > 0 && i + l + 1 < len {
-                candidates.push((i - 1, l + 2));
+        }
+        for j in 1..len-1 {
+            for k in 0..=1 {
+                let mut i = j;
+                let mut l = k;
+                loop {
+                    if bytes[i - 1] != bytes[i + l] {
+                        break;
+                    }
+                    ret += 1;
+                    if i - 1 > 0 && i + l + 1 < len {
+                        i -= 1;
+                        l += 2;
+                    } else {
+                        break;
+                    }
+                }
             }
         }
         ret
