6d2ff359c5240aba8aecae54cbc5ef56b3911346
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-11-15 17:59:59 +0800

    update sliding window solution

--- src/number_of_nice_subarrays.rs
+++ src/number_of_nice_subarrays.rs
@@ -1,6 +1,29 @@
 // leetcode 1248
 pub struct Solution;
 
+// note, because number of odd number only increment by 1
+// so we can use sliding window
+impl Solution {
+    pub fn number_of_subarrays(nums: Vec<i32>, mut k: i32) -> i32 {
+        at_most(&nums, k) - at_most(&nums, k - 1)
+    }
+}
+
+fn at_most(nums: &[i32], mut k: i32) -> i32 {
+    let mut ret = 0;
+    let mut i = 0;
+    for j in 0..nums.len() {
+        k -= nums[j] % 2;
+        while k < 0 {
+            k += nums[i] % 2;
+            i += 1;
+        }
+        ret += j - i + 1;
+    }
+    ret as i32
+}
+
+/*
 impl Solution {
     pub fn number_of_subarrays(nums: Vec<i32>, k: i32) -> i32 {
         let mut map = vec![0; nums.len() + 1];
@@ -17,3 +40,4 @@ impl Solution {
         ret
     }
 }
+*/
