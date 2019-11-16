8b234846a318548fa9f55fcf2d1c8d0d93221754
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-11-17 00:36:34 +0800

    update greedy solution

--- src/find_longest_chain.rs
+++ src/find_longest_chain.rs
@@ -1,5 +1,25 @@
 // leetcode 646
 pub Solution;
+
+// greedy solution
+impl Solution {
+    pub fn find_longest_chain(mut pairs: Vec<Vec<i32>>) -> i32 {
+        pairs.sort_unstable_by_key(|v| v[1]);
+        let mut ret = 1;
+        // sort by last element. thus the end element of Longest chain
+        // is the smallest and can be readily followed
+        let mut last = pairs[0][1];
+        for p in pairs {
+            if last < p[0] {
+                ret += 1;
+                last = p[1];
+            }
+        }
+        ret
+    }
+}
+
+/*
 impl Solution {
     pub fn find_longest_chain(mut pairs: Vec<Vec<i32>>) -> i32 {
         pairs.sort();
@@ -19,3 +39,4 @@ impl Solution {
         dp.into_iter().fold(0, i32::max)
     }
 }
+*/
