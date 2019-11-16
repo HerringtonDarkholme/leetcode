d32ff9083177b90daf3d46fd3f963f565c1a3c68
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-11-16 23:42:49 +0800

    optimize constant time
    * remove boundary check
    * merge loop
    * exploit CPU branch prediction

--- src/max_satisfied.rs
+++ src/max_satisfied.rs
@@ -1,25 +1,24 @@
+// leetcode 1052
 pub struct Solution;
 
 impl Solution {
     pub fn max_satisfied(customers: Vec<i32>, grumpy: Vec<i32>, x: i32) -> i32 {
         let mut total = 0;
-        let mut i = 0;
+        let x = x as usize;
         let mut y = x as usize;
         let mut happy = 0;
-        while y > 0 {
-            total += customers[i] * (1 - grumpy[i]);
-            happy += customers[i] * grumpy[i];
-            i += 1;
-            y -= 1;
+        let mut max = 0;
+
+        for (i, (&c, &g)) in customers.iter().zip(grumpy.iter()).enumerate() {
+            total += c * (1 - g);
+            if y > 0 {
+                y -= 1;
+            } else {
+                max = max.max(happy);
+                happy -= customers[i - x] * grumpy[i - x];
+            }
+            happy += c * g;
         }
-        let mut max = happy;
-        while i < customers.len() {
-            total += customers[i] * (1 - grumpy[i]);
-            happy -= customers[i - x as usize] * grumpy[i - x as usize];
-            happy += customers[i] * grumpy[i];
-            max = max.max(happy);
-            i += 1;
-        }
-        total + max
+        total + max.max(happy)
     }
 }
