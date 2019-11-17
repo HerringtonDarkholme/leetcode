2663bed86e8a3f71882990f959e717794ca7a48c
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-11-17 15:32:32 +0800

    bucket sort/ count sort

--- src/h_index.rs
+++ src/h_index.rs
@@ -3,6 +3,30 @@ pub struct Solution;
 
 impl Solution {
     pub fn h_index(mut citations: Vec<i32>) -> i32 {
+        let len = citations.len();
+        let mut buckets = vec![0; len + 1];
+        // bucket sort, O(n) sort where elements are known in buckets
+        for c in citations {
+            if c > len as i32 {
+                buckets[len] += 1;
+            } else {
+                buckets[c as usize] += 1;
+            }
+        }
+        let mut n = 0;
+        for (c, &num) in buckets.iter().enumerate().rev() {
+            n += num;
+            if n >= c {
+                return c as i32
+            }
+        }
+        0
+    }
+}
+
+/*
+impl Solution {
+    pub fn h_index(mut citations: Vec<i32>) -> i32 {
         citations.sort();
         let mut h = 0;
         let len = citations.len();
@@ -14,3 +38,4 @@ impl Solution {
         h
     }
 }
+*/
