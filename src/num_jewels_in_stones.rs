2de3117d98dd7cfa084cb8401b06b0cd2e2bccb0
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-11-19 14:23:08 +0800

    string + hashset

---
+++ src/num_jewels_in_stones.rs
@@ -0,0 +1,9 @@
+pub struct Solution;
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn num_jewels_in_stones(j: String, s: String) -> i32 {
+        let j: HashSet<_> = j.chars().collect();
+        s.chars().filter_map(|s| j.get(&s).map(|x| 1)).sum()
+    }
+}
