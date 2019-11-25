4ebd5996c61a88fdbe8bdbc411bf4e3c9c9e9a74
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2019-11-25 16:11:35 +0800

    update

--- src/mincost_to_hire_workers.rs
+++ src/mincost_to_hire_workers.rs
@@ -13,19 +13,22 @@ impl Solution {
         unit.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap_or(Equal));
         let mut heap = BinaryHeap::new();
         let mut total_quality = 0;
-        for i in 0..k as usize {
-            let (u, idx) = unit[i];
-            heap.push(quality[idx]);
+        let mut cost = -1.0f64;
+        for (u, idx) in unit {
+            if heap.len() >= k as usize {
+                let q = heap.pop().unwrap();
+                total_quality -= q;
+            }
             total_quality += quality[idx];
-        }
-        let mut cost = total_quality as f64 * unit[k as usize - 1].0;
-        for i in (k as usize)..wage.len() {
-            let (u, idx) = unit[i];
-            let q = heap.pop().unwrap();
-            total_quality -= q;
             heap.push(quality[idx]);
-            total_quality += quality[idx];
-            cost = cost.min(total_quality as f64 * u);
+            if heap.len() == k as usize {
+                cost = if cost > 0.0 {
+                    cost.min(total_quality as f64 * u)
+                } else {
+                    total_quality as f64 * u
+                };
+
+            }
         }
         cost
     }
