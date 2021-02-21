2ac1a4b9f1795b800f6bf04d01f48a74300a7eaa
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-02-21 12:40:01 +0800

    Create 1689_min_partitions.rs

---
+++ 1689_min_partitions.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn min_partitions(n: String) -> i32 {
+        n.chars()
+            .max()
+            .unwrap()
+            .to_digit(10)
+            .unwrap() as i32
+    }
+}
