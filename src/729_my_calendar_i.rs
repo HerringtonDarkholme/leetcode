5910b64da89b908b3a98bbecb91f21d286b3c530
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-06-12 23:47:38 +0800

    update interval tree solution

--- src/729_my_calendar_i.rs
+++ src/729_my_calendar_i.rs
@@ -1,15 +1,66 @@
+struct MyCalendar {
+    root: IntervalTree
+}
+
+trait IntervalTreeOp {
+    fn insert(&mut self, start: i32, end: i32) -> bool;
+}
+
+type IntervalTree = Option<Box<IntervalTreeNode>>;
+
+struct IntervalTreeNode {
+    start: i32,
+    end: i32,
+    left: IntervalTree,
+    right: IntervalTree,
+}
+
+impl IntervalTreeOp for IntervalTree {
+    fn insert(&mut self, start: i32, end: i32) -> bool {
+        if self.is_none() {
+            self.replace(Box::new(IntervalTreeNode{
+                start,
+                end,
+                left: None,
+                right: None,
+            }));
+            return true
+        }
+        let mut n = self.as_mut().unwrap();
+        if end <= n.start {
+            n.left.insert(start, end)
+        } else if start >= n.end {
+            n.right.insert(start, end)
+        } else {
+            false
+        }
+    }
+}
+
+impl MyCalendar {
+    fn new() -> Self {
+        Self {
+            root: None,
+        }
+    }
+    fn book(&mut self, start: i32, end: i32) -> bool {
+        self.root.insert(start, end)
+    }
+}
+
+/*
 use std::collections::BTreeSet;
 
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
 struct MyCalendar {
     start: BTreeSet<i32>,
     end: BTreeSet<i32>,
 }
 
-
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl MyCalendar {
 
     fn new() -> Self {
@@ -18,7 +69,7 @@ impl MyCalendar {
             end: BTreeSet::new(),
         }
     }
-    
+
     fn book(&mut self, start: i32, end: i32) -> bool {
         let nearest_start = self.start.range(..end).rev().next();
         let nearest_end = self.end.range(start+1..).next();
@@ -31,7 +82,7 @@ impl MyCalendar {
                 if n_end > start {
                     return false
                 }
-            } 
+            }
             _ => (),
         }
         self.start.insert(start);
@@ -39,6 +90,7 @@ impl MyCalendar {
         true
     }
 }
+*/
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 mod util;
-#[path="./679_judge_point24.rs"]
+#[path="./729_my_calendar_i.rs"]
 pub mod max_ice_cream;
 
 fn main() {
