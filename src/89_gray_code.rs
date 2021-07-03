0b29d6db7e21bfe654a48b8fbaac3efa436b8949
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-07-03 20:05:44 +0800

    Update 89_gray_code.rs

--- src/89_gray_code.rs
+++ src/89_gray_code.rs
@@ -1,21 +1,18 @@
 impl Solution {
     pub fn gray_code(n: i32) -> Vec<i32> {
-        let mut dp = vec![vec![0, 1]];
-        gray_code(n as usize - 1, &mut dp)
+        gray_code(n)
     }
 }
 
-fn gray_code(n: usize, dp: &mut Vec<Vec<i32>>) -> Vec<i32> {
-    if dp.len() > n {
-        return dp[n].clone()
+fn gray_code(n: i32) -> Vec<i32> {
+    let mut r = vec![0, 1];
+    for i in 1..n {
+        let rev: Vec<_> = r
+            .iter()
+            .rev()
+            .map(|e| e | 1 << i)
+            .collect();
+        r.extend(rev);
     }
-    let mut n1 = gray_code(n - 1, dp);
-    let mut n2: Vec<_> = dp[n - 1]
-        .iter()
-        .map(|e| e | 1 << n)
-        .collect();
-    n2.reverse();
-    n1.extend(n2);
-    dp.push(n1);
-    dp[n].clone()
+    r
 }
