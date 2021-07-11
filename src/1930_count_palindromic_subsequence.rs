972df2bfd0d25d2632608a3464e5fb05b2f3f1db
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-07-11 16:21:52 +0800

    update comments

--- src/1930_count_palindromic_subsequence.rs
+++ src/1930_count_palindromic_subsequence.rs
@@ -16,11 +16,16 @@ impl Info {
     }
 }
 
+// a new char should count three parts
+// as the first char in a new palindrome
+// as the mid char in a palindrome
+// as the final char in a palindrome
 impl Solution {
     pub fn count_palindromic_subsequence(s: String) -> i32 {
         let mut ret = vec![Info::new(); 26];
         let mut r = 0;
         for i in s.as_bytes().into_iter().map(|b| (b-'a' as u8) as usize) {
+            // run finalizer first
             let mut info = &mut ret[i];
             for j in 0..N {
                 if info.mid[j] {
@@ -29,12 +34,14 @@ impl Solution {
                     r += 1;
                 }
             }
+            // then mid
             for info in ret.iter_mut() {
                 if !info.met || info.used[i] {
                     continue;
                 }
                 info.mid[i] = true;
             }
+            // finally initializer
             let mut info = &mut ret[i];
             if !info.met {
                 info.met = true;
