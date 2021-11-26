9f1a0cf3171292c2690e8f3135f39622b51ea975
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-11-26 02:04:58 -0500

    update

--- src/986_interval_intersection.rs
+++ src/986_interval_intersection.rs
@@ -37,3 +37,34 @@ fn test() {
     let expect = vec![vec![1,2], vec![5, 5], vec![8, 10], vec![15, 23], vec![24, 24], vec![25, 25]];
     assert_eq!(Solution::interval_intersection(a, b), expect);
 }
+
+/*
+impl Solution {
+    pub fn interval_intersection(first_list: Vec<Vec<i32>>, second_list: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut v = vec![];
+    helper(&first_list, &second_list, &mut v);
+    v
+    }
+}
+
+fn helper<'a>(mut l1: &'a[Vec<i32>], mut l2: &'a[Vec<i32>], dest: &'a mut Vec<Vec<i32>>) {
+    if l1.is_empty() || l2.is_empty() {
+        return;
+    }
+    let mut a = &l1[0];
+    let mut b = &l2[0];
+    if a[0] > b[0] {
+        std::mem::swap(&mut a, &mut b);
+        std::mem::swap(&mut l1, &mut l2);
+    }
+    if a[1] < b[0] {
+        return helper(&l1[1..], l2, dest);
+    }
+    if a[1] >= b[0] && a[1] <= b[1] {
+        dest.push(vec![b[0], a[1]]);
+        return helper(&l1[1..], l2, dest);
+    }
+    dest.push(vec![b[0], b[1]]);
+    helper(l1, &l2[1..], dest);
+}
+*/
