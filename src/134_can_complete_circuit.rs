9c072f6c9891e160dd46158a191e4854cda07f76
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-01-20 22:53:46 -0500

    Update 134_can_complete_circuit.rs

--- src/134_can_complete_circuit.rs
+++ src/134_can_complete_circuit.rs
@@ -24,3 +24,28 @@ impl Solution {
         -1
     }
 }
+
+/*
+
+impl Solution {
+    pub fn can_complete_circuit(mut gas: Vec<i32>, mut cost: Vec<i32>) -> i32 {
+        let len = gas.len();
+        let (mut candidate, mut credit, mut debit) = (-1, 0, 0);
+        for (i, (g, c)) in gas.into_iter().zip(cost.into_iter()).enumerate() {
+            credit += g - c;
+            if credit < 0 {
+                candidate = -1;
+                debit -= credit;
+                credit = 0;
+            } else if candidate < 0 {
+                candidate = i as i32;                
+            }
+        }
+        if credit >= debit {
+            candidate
+        } else {
+            -1
+        }
+    }
+}
+*/
