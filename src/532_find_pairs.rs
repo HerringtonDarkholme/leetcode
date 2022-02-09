24e29027869f42dcf277162eca480f6c50efe098
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-02-08 22:48:03 -0500

    Update 532_find_pairs.rs

--- src/532_find_pairs.rs
+++ src/532_find_pairs.rs
@@ -16,3 +16,27 @@ impl Solution {
         ret.len() as i32
     }
 }
+
+/*
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn find_pairs(mut nums: Vec<i32>, k: i32) -> i32 {
+        let mut map = HashMap::new();
+        for n in nums {
+            *map.entry(n).or_insert(0) += 1;
+        }
+        if k == 0 {
+            return map.values().filter(|&&v| v > 1).count() as i32
+        }
+        let mut ret = 0;
+        for (n ,v) in map.iter() {
+            if map.contains_key(&(n + k)) {
+                ret += 1;
+            }
+        }
+        ret
+    }
+}
+
+*/
