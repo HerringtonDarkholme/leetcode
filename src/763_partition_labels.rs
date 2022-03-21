7b9bfdc7cf9a7d519ab52deb0b95cc96881a7b43
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-03-20 22:57:17 -0400

    update

--- src/763_partition_labels.rs
+++ src/763_partition_labels.rs
@@ -31,3 +31,29 @@ impl Solution {
         ret
     }
 }
+
+/*
+impl Solution {
+    pub fn partition_labels(s: String) -> Vec<i32> {
+        let s: Vec<_> = s.bytes().collect();
+        let mut occ = vec![0; 26];
+        for i in 0..s.len() {
+            let c = (s[i] - b'a') as usize;
+            occ[c] = i;
+        }
+        let mut ret = vec![];
+        let mut start = 0;
+        let mut max = 0;
+        for i in 0..s.len() {
+            let c = (s[i] - b'a') as usize;
+            max = max.max(occ[c]);
+            if max == i {
+                ret.push((i - start + 1) as i32);
+                start = max + 1;
+                max = max + 1;
+            }
+        }
+        ret
+    }
+}
+*/
