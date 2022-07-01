3adbc7ab1b77d2d82a81d18273e624daff4fa0ef
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-06-30 21:57:08 -0400

    update

--- src/1710_maximum_units.rs
+++ src/1710_maximum_units.rs
@@ -16,3 +16,18 @@ impl Solution {
         r
     }
 }
+
+/*
+impl Solution {
+    pub fn maximum_units(mut box_types: Vec<Vec<i32>>, mut truck_size: i32) -> i32 {
+        box_types.sort_unstable_by_key(|v| -v[1]);
+        let mut ret = 0;
+        for box_type in box_types.into_iter() {
+            let loaded_count = box_type[0].min(truck_size);
+            ret += loaded_count * box_type[1];
+            truck_size -= loaded_count;
+        }
+        ret
+    }
+}
+*/
