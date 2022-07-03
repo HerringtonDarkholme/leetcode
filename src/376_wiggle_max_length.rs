3feb374420c0abf3a5e139aae6455ead95152c2b
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-07-03 01:02:43 -0400

    update

--- src/376_wiggle_max_length.rs
+++ src/376_wiggle_max_length.rs
@@ -30,3 +30,22 @@ impl Solution {
         inc.max(des)
     }
 }
+
+/*
+impl Solution {
+    pub fn wiggle_max_length(nums: Vec<i32>) -> i32 {
+        let mut up = 1;
+        let mut down = 1;
+        for i in 1..nums.len() {
+            if nums[i] > nums[i -1] {
+                up = down + 1;
+            } else if nums[i] < nums[i - 1] {
+                down = up + 1;
+            }
+        }
+        up.max(down)
+    }
+}
+// inc 1 17 5
+// dec 17 5
+*/
