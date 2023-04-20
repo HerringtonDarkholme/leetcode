edac1979a647b158d816134f1628d3ce28905807
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-04-19 20:31:53 -0400

    Update 662_width_of_binary_tree.rs

--- src/662_width_of_binary_tree.rs
+++ src/662_width_of_binary_tree.rs
@@ -50,3 +50,60 @@ fn span(root: Option<Rc<RefCell<TreeNode>>>) -> Option<i32> {
     }
     Some(max as i32 + 1)
 }
+/*
+    
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn width_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        width(root)
+    }
+}
+
+fn width(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    let mut queue = vec![(0, root.unwrap())];
+    let mut next = vec![];
+    let mut ret = 0;
+    while !queue.is_empty() {
+        let max = queue.last().unwrap().0 - queue.first().unwrap().0 + 1;
+        ret = ret.max(max);
+        for (i, mut node) in queue {
+            let mut n = node.borrow_mut();
+            if let Some(left) = n.left.take() {
+                next.push((2 * i, left));
+            }
+            if let Some(right) = n.right.take() {
+                next.push((2 * i + 1, right));
+            }
+        }
+        queue = next;
+        next = vec![];
+    }
+    ret
+}
+
+// parent index = n
+// left_child index = 2 * n 
+// right_child index = 2 * n + 1
+// max width = queue[0] 
+    
+    
+    */ 
