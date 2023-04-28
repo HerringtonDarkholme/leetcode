4e746243120fbb6d0045af29591d76e48d91070a
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-04-28 03:37:30 -0400

    Update and rename num_similar_group.rs to 839_num_similar_group.rs

--- src/839_num_similar_group.rs
+++ src/839_num_similar_group.rs
@@ -1,3 +1,56 @@
+impl Solution {
+    pub fn num_similar_groups(strs: Vec<String>) -> i32 {
+        let strs = strs.iter().map(|s| s.as_bytes()).collect::<Vec<_>>();
+        let mut parents: Vec<_> = (0..strs.len()).collect();
+        for i in 1..strs.len() {
+            for j in 0..i {
+                if is_same(&strs[i], &strs[j]) {
+                    union(i, j, &mut parents);
+                }
+            }
+        }
+        let mut ret = 0;
+        for i in 0..parents.len() {
+            if i == parents[i] {
+                ret += 1;
+            }
+        }
+        ret
+    }
+}
+
+fn is_same(a: &[u8], b: &[u8]) -> bool {
+    let mut i = 0;
+    for (&a, &b) in a.iter().zip(b.iter()) {
+        if a != b {
+            i += 1;
+        }
+        if i > 2 {
+            return false;
+        }
+    }
+    i <= 2 
+}
+
+fn find(mut a: usize, parents: &[usize]) -> usize {
+    while a != parents[a] {
+        a = parents[a];
+    }
+    a
+}
+
+fn union(a: usize, b: usize, parents: &mut [usize]) {
+    let a = find(a, parents);
+    let b = find(b, parents);
+    if a < b {
+        parents[a] = b;
+    } else {
+        parents[b] = a;
+    }
+}
+
+
+
 use std::collections::HashSet;
 impl Solution {
     pub fn num_similar_groups(a: Vec<String>) -> i32 {
