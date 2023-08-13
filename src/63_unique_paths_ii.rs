c98b16865b40e198c07981ab34426a03de36a0c1
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-08-12 22:45:16 -0400

    Update and rename unique_paths_ii.rs to 63_unique_paths_ii.rs

--- src/63_unique_paths_ii.rs
+++ src/63_unique_paths_ii.rs
@@ -1,4 +1,18 @@
-pub struct Solution;
+impl Solution {
+    pub fn unique_paths_with_obstacles(grid: Vec<Vec<i32>>) -> i32 {
+        let mut paths = vec![0; grid[0].len() + 1];
+        paths[1] = 1;
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                paths[j + 1] += paths[j];
+                paths[j + 1] *= 1 - grid[i][j];
+            }
+        }
+        paths[paths.len() - 1]
+    }
+}
+
+/*pub struct Solution;
 
 impl Solution {
     pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
@@ -27,3 +41,4 @@ impl Solution {
         paths[row - 1][col - 1]
     }
 }
+*/
