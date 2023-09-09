8e3a368e522762aa7f7e76a4b060a30db2f01c56
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-09-09 04:24:37 -0400

    Update 377_combination_sum4.rs

--- src/377_combination_sum4.rs
+++ src/377_combination_sum4.rs
@@ -1,5 +1,21 @@
 impl Solution {
     pub fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
+        let target = target as usize;
+        let mut dp = vec![0; target + 1];
+        dp[0] = 1;
+        for i in 1..=target {
+            for &n in &nums {
+                if i >= n as usize {
+                    dp[i] += dp[i - n as usize];
+                }
+            }
+        }
+        dp[target]
+    }
+}
+/*
+impl Solution {
+    pub fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
         let nums: Vec<_> = nums.into_iter().map(|n| n as usize).collect();
         let mut dp = vec![-1; 1001];
         sum(&nums, target as usize, &mut dp)
@@ -22,3 +38,4 @@ fn sum(nums: &[usize], target: usize, dp: &mut Vec<i32>) -> i32 {
     dp[target] = ways;
     ways
 }
+*/
