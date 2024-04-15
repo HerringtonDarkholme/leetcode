e087fee0dde173541e0217b6fbd4913059e74a92
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-04-14 23:13:05 -0700

    Update 129_sum_numbers.rs

--- src/129_sum_numbers.rs
+++ src/129_sum_numbers.rs
@@ -29,3 +29,26 @@ impl Solution {
         }
     }
 }
+
+/*
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn sum_numbers(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut ret = 0;
+        sum_numbers(root, 0, &mut ret);
+        ret
+    }
+}
+
+fn sum_numbers(node: Option<Rc<RefCell<TreeNode>>>, acc: i32, sum: &mut i32) -> bool {
+    let Some(node) = node else { return false; };
+    let mut n = node.borrow_mut();
+    let acc = acc * 10 + n.val;
+    let l = sum_numbers(n.left.take(), acc, sum);
+    let r = sum_numbers(n.right.take(), acc, sum);
+    if !l && !r { *sum += acc; }
+    true
+}
+
+*/
