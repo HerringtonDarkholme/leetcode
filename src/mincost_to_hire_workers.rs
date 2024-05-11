0ec7b66caa5499f1b014dd00c8a61ab0376ebe1f
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-05-11 00:40:03 -0700

    Update mincost_to_hire_workers.rs

--- src/mincost_to_hire_workers.rs
+++ src/mincost_to_hire_workers.rs
@@ -1,4 +1,34 @@
 use std::collections::BinaryHeap;
+impl Solution {
+    pub fn mincost_to_hire_workers(quality: Vec<i32>, wage: Vec<i32>, k: i32) -> f64 {
+        let mut included = BinaryHeap::new();
+        let mut candidates: Vec<_> = (0..quality.len()).collect();
+        // max efficiency -> min efficiency
+        candidates.sort_unstable_by(|&a, &b| {
+            (wage[b] * quality[a]).cmp(&(wage[a] * quality[b]))
+        });
+        let mut max_efficiency = 0.0;
+        let mut total_quality = 0.0;
+        for _ in 0..k { // build initial included
+            let i = candidates.pop().unwrap();
+            max_efficiency = wage[i] as f64 / quality[i] as f64;
+            included.push(quality[i]);
+            total_quality += quality[i] as f64;
+        }
+        let mut min = max_efficiency * total_quality;
+        while let Some(i) = candidates.pop() {
+            max_efficiency = wage[i] as f64 / quality[i] as f64;
+            included.push(quality[i]);
+            let removed = included.pop().unwrap();
+            total_quality += (quality[i] - removed) as f64;
+            min = min.min(max_efficiency * total_quality);
+        }
+        min
+    }
+}
+
+/*
+use std::collections::BinaryHeap;
 use std::cmp::Ordering::Equal;
 
 pub struct Solution;
@@ -33,3 +63,4 @@ impl Solution {
         cost
     }
 }
+*/
