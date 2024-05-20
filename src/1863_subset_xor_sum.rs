769444b0d2b5f81e833f99c6bf907a35d23dcf69
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-05-19 18:57:45 -0700

    Update 1863_subset_xor_sum.rs

--- src/1863_subset_xor_sum.rs
+++ src/1863_subset_xor_sum.rs
@@ -14,3 +14,21 @@ impl Solution {
         sum
     }
 }
+/*
+impl Solution {
+    pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+        let mut count = vec![0; 32];
+        count[0] = 1;
+        let mut ret = 0usize;
+        for num in nums.into_iter().map(|n| n as usize) {
+            let mut next = count.clone();
+            for (i, cnt) in count.into_iter().enumerate() {
+                ret += (i ^ num) * cnt;
+                next[i ^ num] += cnt;
+            }
+            count = next;
+        }
+        ret as i32
+    }
+}
+*/
